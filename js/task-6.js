907d46b5471d88d6dfd3ee29b26bfb89372ff2b3
Yevhen Nesvit <yevhen.nesvit@gmail.com>
2024-06-22 16:18:40 +0300

    add task-6 solution

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const boxesDiv = document.querySelector("#boxes");
+
+  boxesDiv.innerHTML = "";
+
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesDiv.appendChild(box);
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  const boxesDiv = document.querySelector("#boxes");
+  boxesDiv.innerHTML = "";
+}
+
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const input = document.querySelector("input");
+
+createBtn.addEventListener("click", () => {
+  const amount = parseInt(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = "";
+  }
+});
+
+destroyBtn.addEventListener("click", () => {
+  destroyBoxes();
+});
